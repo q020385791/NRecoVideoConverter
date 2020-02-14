52f53e77a2cc2f87870c6cd95cd3b74e66b66aa2
sai wanza <q020385791@gmail.com>
2020-02-14 13:38:58 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace NRecoVideoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SourceFile = new System.Windows.Forms.Button();
+            this.TargetFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(45, 222);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(110, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // SourceFile
+            // 
+            this.SourceFile.Location = new System.Drawing.Point(43, 27);
+            this.SourceFile.Name = "SourceFile";
+            this.SourceFile.Size = new System.Drawing.Size(94, 23);
+            this.SourceFile.TabIndex = 1;
+            this.SourceFile.Text = "選擇來源檔案";
+            this.SourceFile.UseVisualStyleBackColor = true;
+            this.SourceFile.Click += new System.EventHandler(this.SourceFile_Click);
+            // 
+            // TargetFile
+            // 
+            this.TargetFile.Location = new System.Drawing.Point(43, 98);
+            this.TargetFile.Name = "TargetFile";
+            this.TargetFile.Size = new System.Drawing.Size(135, 23);
+            this.TargetFile.TabIndex = 2;
+            this.TargetFile.Text = "選擇放置位置與檔案名";
+            this.TargetFile.UseVisualStyleBackColor = true;
+            this.TargetFile.Click += new System.EventHandler(this.TargetFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(207, 149);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "要轉成什麼類型的檔案";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 272);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TargetFile);
+            this.Controls.Add(this.SourceFile);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button SourceFile;
+        private System.Windows.Forms.Button TargetFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NRecoVideoConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string sSourceFile;
+        public string sTargetFile;
+        delegate void _Convert();
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(sSourceFile)&& !string.IsNullOrEmpty(sTargetFile))
+            {
+                //_Convert DelConvert = new _Convert(Convert);
+                //DelConvert.Invoke();
+                Thread t1 = new Thread(Convert);
+                t1.Start();
+                btnConvert.Enabled = false;
+                TargetFile.Enabled = false;
+                SourceFile.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("請先決定來源與目標檔案位置名稱");
+            }
+
+        }
+       
+        private void SourceFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenDialog = new OpenFileDialog();
+
+            if (OpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                sSourceFile = OpenDialog.FileName;
+                label1.Text = sSourceFile;
+            }
+            TargetFile.Enabled = true;
+        }
+
+        private void TargetFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDialog = new SaveFileDialog();
+
+            if (SaveDialog.ShowDialog() == DialogResult.OK)
+            {
+                sTargetFile = SaveDialog.FileName;
+                label2.Text = sTargetFile;
+            }
+            btnConvert.Enabled = true;
+        }
+        public static FieldInfo[] fieldInfos = typeof(Format).GetFields(BindingFlags.Public |
+         BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+        public List<FieldInfo> VideoTypes = fieldInfos.Where(fi => fi.IsLiteral && !fi.IsInitOnly).ToList();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var test = typeof(Format).GetProperties();
+            foreach (var item in VideoTypes)
+            {
+                comboBox1.Items.Add(item.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            TargetFile.Enabled = false;
+            btnConvert.Enabled = false;
+        }
+
+
+        string Combox1SelectItem;
+        public void GetComboBoxItem()
+        {
+            Combox1SelectItem = (string)comboBox1.SelectedItem;
+        }
+        public void Convert()
+        {
+            var ffMpeg = new FFMpegConverter();
+
+            comboBox1.InvokeIfRequired(GetComboBoxItem);
+            ConvertSettings setting = new ConvertSettings();
+            //string sTargetType = (string)comboBox1.SelectedItem;
+            string sTargetType = Combox1SelectItem;
+            FieldInfo Info = VideoTypes.Where(x => x.Name == sTargetType).Select(y => y).FirstOrDefault();
+            setting.AudioSampleRate = 44100;
+            label4.InvokeIfRequired(() =>
+            {
+                label4.ForeColor = Color.Red;
+                label4.Text = "轉檔中 請稍後";
+            });
+            try
+            {
+                ffMpeg.ConvertMedia(sSourceFile, null, sTargetFile + "." + Info.Name, Info.GetValue(null).ToString(), setting);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("轉檔失敗");
+            }
+
+
+            Console.WriteLine("Finish");
+
+            //lambda非同步UI更新
+            btnConvert.InvokeIfRequired(() =>
+            {
+                btnConvert.Enabled = true;
+            });
+            TargetFile.InvokeIfRequired(() =>
+            {
+                TargetFile.Enabled = false;
+            });
+
+            label4.InvokeIfRequired(() =>
+            {
+                label4.Text = "轉檔完畢，可以繼續進行下個轉換";
+            });
+        }
+
+    }
+    //擴充方法
+    public static class Extension
+    {
+        //非同步委派更新UI
+        public static void InvokeIfRequired(
+            this Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)//在非當前執行緒內 使用委派
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+
+   
+}
